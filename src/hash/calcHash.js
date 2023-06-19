b14ef5ca70503ef46460792f1e1d3fe6145970c0
Faz-R <FazaR92@gmail.com>
2023-06-19 16:30:44 +0300

    feat: add hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,12 @@
+import { createHash } from "crypto";
+import { readFile } from "fs/promises";
+
 const calculateHash = async () => {
-    // Write your code here 
+  const buff = await readFile(
+    new URL("./files/fileToCalculateHashFor.txt", import.meta.url)
+  );
+  const hash = createHash("sha256").update(buff).digest("hex");
+  console.log(hash);
 };
 
 await calculateHash();
