ade552a40f9cefad8e075abea846f1fa13412e39
Faz-R <FazaR92@gmail.com>
2023-06-19 16:02:08 +0300

    feat: add cli and modules

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,12 @@
 const parseArgs = () => {
-    // Write your code here 
+  const args = process.argv.slice(2);
+  const result = args.reduce((sum, elem, index, array) => {
+    if (elem.startsWith("--")) {
+      sum.push(`${elem.slice(2)} is ${array[index + 1]}`);
+    }
+    return sum;
+  }, []);
+  console.log(result.join(", "));
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,12 @@
 const parseEnv = () => {
-    // Write your code here 
+  const keys = Object.keys(process.env).filter((key) => key.startsWith("RSS_"));
+
+  let output = "";
+  keys.forEach((key) => {
+    output += `RSS_${key.slice("RSS_".length)}=${process.env[key]}; `;
+  });
+
+  console.log(output);
 };
 
 parseEnv();
