1eb418e8a506d7fc0618a79378d7674667df7426
Faz-R <FazaR92@gmail.com>
2023-06-19 19:10:45 +0300

    feat: add zip

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,20 @@
+import { pipeline } from "node:stream";
+import { createReadStream, createWriteStream } from "node:fs";
+import { createGzip } from "node:zlib";
+
 const compress = async () => {
-    // Write your code here 
+  pipeline(
+    createReadStream(new URL("./files/fileToCompress.txt", import.meta.url)),
+    createGzip(),
+    createWriteStream(new URL("./files/archive.gz", import.meta.url)),
+    (err) => {
+      if (err) {
+        console.error("Pipeline failed.", err);
+      } else {
+        console.log("Pipeline succeeded.");
+      }
+    }
+  );
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,20 @@
+import { pipeline } from "node:stream";
+import { createReadStream, createWriteStream } from "node:fs";
+import { createGunzip } from "node:zlib";
+
 const decompress = async () => {
-    // Write your code here 
+  pipeline(
+    createReadStream(new URL("./files/archive.gz", import.meta.url)),
+    createGunzip(),
+    createWriteStream(new URL("./files/fileToCompress.txt", import.meta.url)),
+    (err) => {
+      if (err) {
+        console.error("Pipeline failed.", err);
+      } else {
+        console.log("Pipeline succeeded.");
+      }
+    }
+  );
 };
 
 await decompress();
